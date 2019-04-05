67099a8bb756d5fab1268d5ad34fe740d7cd36a5
NET\flobo <fluvio.lobo@gmail.com>
2019-04-05 16:03:41 -0400

    Completed and tested a new version of the autoinstall batch script capable of determining if the user has admin rights (within the context of the installation of Houdini assets)

--- autoinstall_old.bat
+++ autoinstall_old.bat
@@ -2,42 +2,24 @@ cls
 ECHO OFF
 color 02
 :: This program automatically installs the python requirements for the Houdini DICOM Toolset
-:: This program must be executed from the terminal and DOES NOT require admin priviledges
-::
-::	Comments:
-::		- Working with wildcards to disregard Houdini's versioning
-::		- Working with variable names for paths
+:: This program must be executed from the terminal, with administrator rights
 ECHO ----------------------------------------------------
 ECHO Installing Houdini DICOM Toolset Python Requirements
 ECHO ----------------------------------------------------
 :: This version of the application has a hard-coded Houdini version, needs to be improved...
-
-:: [0] Set variables
-set origin_path=%cd%
-cd C:\Apps\Side Effects Software\Houdini*\python27\
-set target_path=%cd%
-ECHO The origin path is = %origin_path% 
-ECHO The target path is = %target_path%
-cd %origin_path%
-
 :: [1] The program copies copies the list of requirements to the houdini-python directory
-copy requirements.txt "%target_path%\requirements.txt"
-
+copy requirements.txt "C:\Program Files\Side Effects Software\Houdini*\python27\requirements.txt"
 :: [2] The program navigates to the houdini-python directory
-cd %target_path%
-
+cd C:\Program Files\Side Effects Software\Houdini*\python27
 :: [2] The program installs pip
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python get-pip.py
-
 :: [3] The program installs the requirements
 python -m pip install -r requirements.txt
-
 :: [4] Clean-up
 del requirements.txt
-cd %origin_path%
-
-:: DONE!
+cd C:\Users\flobo\Documents\Gits\PD3D\Houdini-DICOM-Toolset
 ECHO ----------------------------------------------------
 ECHO Installation Completed!
 ECHO ----------------------------------------------------
+
